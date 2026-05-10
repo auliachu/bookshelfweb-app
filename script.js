e17bdc9d5b9f252dba6732c537af32f2f2d27eec
auliachu <aulisals2002@gmail.com>
2026-05-10 08:35:57 +0700

    Book Cover Update

--- script.js
+++ script.js
@@ -4,84 +4,84 @@ const books = [
         id: 1,
         title: "Ajaran Bahagia dari Jawa",
         cover: "assets/images/bukubahagiadarijawa.png",
-        landscape: "assets/images/ajaranbahagiajawalandscape.webp",
+        landscape: "assets/images/jawald.jpg",
         portraits: [
-            "assets/images/potraitjawa.jpg",
-            "assets/images/potraitjawa2.jpeg",
-            "assets/images/potraitjawa3.jpeg"
+            "assets/images/jawa2.jpg",
+            "assets/images/bukubahagiadarijawa.png",
+            "assets/images/jawa1.png"
         ],
-        desc: "Sebuah perpustakaan di antara kehidupan dan kematian.",
-        review: "Buku yang sangat menginspirasi. Membaca ini membuat saya berpikir ulang tentang penyesalan dan pilihan hidup. Plotnya sangat unik dan penyampaian penulis sangat emosional namun menenangkan.",
+        desc: "Setelah lelah mencari bahagia dimana mana, cobalah kembali pada ajaran leluhur Jawa. <br><br> After growing tired of searching for happiness everywhere, try returning to the teachings of Javanese ancestors.",
+        review: "Buku Ajaran Bahagia dari Jawa karya Paksi Raras Alit mengajak pembaca untuk kembali memahami kebijaksanaan leluhur Jawa sebagai sumber kebahagiaan yang sering terlupakan di tengah kehidupan modern yang serba cepat. Melalui refleksi yang mendalam namun disampaikan dengan gaya ringan, buku ini menekankan bahwa kebahagiaan sejati tidak terletak pada pencapaian materi, melainkan pada ketenangan batin, keseimbangan hidup, dan kemampuan memahami diri sendiri. Nilai-nilai seperti legawa, olah rasa, serta keselarasan dalam berpikir dan bertindak diangkat sebagai pedoman untuk menghadapi tekanan hidup masa kini, sehingga pembaca dapat menemukan kebahagiaan yang lebih sederhana, mendalam, dan berkelanjutan. <br><br> The book <i>Ajaran Bahagia dari Jawa</i> by Paksi Raras Alit invites readers to rediscover the wisdom of Javanese ancestors as a source of happiness that is often forgotten in today’s fast-paced modern life. Through deep reflection presented in a light and accessible style, the book emphasizes that true happiness does not lie in material achievements, but in inner peace, balance in life, and the ability to understand oneself. Values such as legawa (sincere acceptance), olah rasa (refined self-awareness), and harmony in thought and action are highlighted as guiding principles for facing modern life’s pressures, helping readers find a form of happiness that is simpler, deeper, and more sustainable.",
         isFavorite: false
     },
     {
         id: 2,
         title: "Hari Ini Aku Datang Kembali Ke Toko Buku Kobayashi",
         cover: "assets/images/bukukobayashi.jpg",
-        landscape: "hassets/images/bukukobayashi.jpg",
+        landscape: "assets/images/kobayashild.webp",
         portraits: [
-            "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?auto=format&fit=crop&q=80&w=300",
-            "https://images.unsplash.com/photo-1532012197267-da84d127e765?auto=format&fit=crop&q=80&w=300",
-            "https://images.unsplash.com/photo-1495446815901-a7297e633e8d?auto=format&fit=crop&q=80&w=300"
+            "assets/images/kobayashi1.jpg",
+            "assets/images/bukukobayashi.jpg",
+            "assets/images/kobayashi2.jpg"
         ],
-        desc: "Perubahan kecil yang memberikan hasil luar biasa.",
-        review: "Panduan praktis terbaik untuk membangun kebiasaan baik dan menghilangkan yang buruk. Sangat aplikatif dan didasarkan pada ilmu psikologi yang mudah dicerna.",
+        desc: "Sebuah kisah nyata tentang bagaimana Oomori Rika berkembang setelah bertemu dengan Yumiko Kobayashi. <br><br> A true story about how Oomori Rika grows and transforms after meeting Yumiko Kobayashi.",
+        review: "Buku Hari Ini Aku Datang Kembali ke Toko Buku Kobayashi karya Tetsuya Kawakami menceritakan perjalanan Oomori Rika, seorang karyawan baru di perusahaan distribusi buku yang harus beradaptasi dengan kehidupan dan pekerjaan barunya di Osaka. Dalam masa penuh keraguan dan ketidakpercayaan diri, Rika bertemu dengan Yumiko Kobayashi, pemilik toko buku kecil berusia puluhan tahun yang hangat dan penuh cerita. Melalui kunjungan berulang ke toko tersebut, Rika mendengarkan berbagai kisah hidup yang perlahan membantunya memahami diri sendiri, menemukan keberanian, dan melihat kelemahan sebagai bagian berharga dari hidup. Novel yang terinspirasi dari kisah nyata ini menghadirkan suasana hangat dan reflektif, serta menyampaikan pesan bahwa setiap orang bisa tumbuh dan menemukan arah hidupnya melalui pengalaman sederhana dan pertemuan yang bermakna. <br><br> The book Hari Ini Aku Datang Kembali ke Toko Buku Kobayashi by Tetsuya Kawakami tells the story of Oomori Rika, a new employee at a book distribution company who must adapt to her new life and work in Osaka. During a time filled with doubt and insecurity, Rika meets Yumiko Kobayashi, the warm and wise owner of a long-established small bookstore. Through her repeated visits to the shop, Rika listens to various life stories that gradually help her understand herself, find courage, and see her weaknesses as a valuable part of life. Inspired by a true story, the novel offers a warm and reflective atmosphere, conveying the message that everyone can grow and discover their path through simple experiences and meaningful encounters.",
         isFavorite: true
     },
     {
         id: 3,
         title: "Filosofi Teras",
         cover: "assets/images/bukufilosofiteras.avif",
-        landscape: "assets/images/bukufilosofiteras.avif",
+        landscape: "assets/images/filosofiterasld.jpg",
         portraits: [
-            "https://images.unsplash.com/photo-1494809610410-160faaed4de0?auto=format&fit=crop&q=80&w=300",
-            "https://images.unsplash.com/photo-1519681393784-d120267933ba?auto=format&fit=crop&q=80&w=300",
-            "https://images.unsplash.com/photo-1522205408450-add114ad53fe?auto=format&fit=crop&q=80&w=300"
+            "assets/images/filosofiteras1.jpg",
+            "assets/images/bukufilosofiteras.avif",
+            "assets/images/filosofiteras2.jpg"
         ],
-        desc: "Rahasia Jepang untuk umur panjang dan bahagia.",
-        review: "Buku yang hangat dan menenangkan. Memberikan perspektif baru tentang bagaimana menemukan tujuan hidup dari hal-hal sederhana setiap harinya.",
+        desc: "Manusia tidak memiliki kuasa untuk memiliki apapun yang dia mau, tetapi dia memiliki kuasa untuk tidak mengingini apa yang dia belum miliki, dan dengan gembira memaksimalkan apa yang dia terima. <br><br> Humans do not have the power to possess everything they desire, but they do have the power to not crave what they do not yet have, and to joyfully make the most of what they have received.",
+        review: "Buku Filosofi Teras karya Henry Manampiring mengangkat ajaran Stoisisme, yaitu filsafat Yunani-Romawi kuno yang berfokus pada pengendalian emosi dan pembentukan mental yang tangguh dalam menghadapi kehidupan. Buku ini menjelaskan bahwa banyak emosi negatif seperti marah, cemas, dan sedih bukan berasal dari peristiwa itu sendiri, melainkan dari cara kita memaknainya, sehingga kita diajak untuk lebih fokus pada hal-hal yang bisa dikendalikan dan menerima yang tidak bisa diubah. Dengan gaya penulisan yang ringan, praktis, dan relevan bagi kehidupan modern, buku ini membantu pembaca memahami cara berpikir yang lebih rasional, tenang, dan bijak dalam menghadapi berbagai situasi hidup, sekaligus membuktikan bahwa filsafat tidak harus rumit, tetapi bisa menjadi panduan nyata untuk menjalani hidup yang lebih damai dan seimbang. <br><br> The book <i>Filosofi Teras</i> by Henry Manampiring introduces the teachings of Stoicism, an ancient Greco-Roman philosophy that focuses on emotional control and building a resilient mindset in facing life. It explains that many negative emotions—such as anger, anxiety, and sadness—do not arise from events themselves, but from how we interpret them, encouraging readers to focus on what they can control and to accept what cannot be changed. Written in a light, practical, and modern style, the book helps readers develop a more rational, calm, and wise way of thinking when dealing with life’s challenges, while demonstrating that philosophy does not have to be complicated, but can serve as a practical guide to living a more peaceful and balanced life.",
         isFavorite: false
     },
     {
         id: 4,
         title: "Gentayangan",
         cover: "assets/images/bukugentayangan.jpg",
-        landscape: "assets/images/bukugentayangan.jpg",
+        landscape: "assets/images/gentayanganld.webp",
         portraits: [
-            "https://images.unsplash.com/photo-1474366521946-c3d4b507abf2?auto=format&fit=crop&q=80&w=300",
-            "https://images.unsplash.com/photo-1507842217343-583bb7270b66?auto=format&fit=crop&q=80&w=300",
-            "https://images.unsplash.com/photo-1481627834876-b7833e8f5570?auto=format&fit=crop&q=80&w=300"
+            "assets/images/gentayangan1.webp",
+            "assets/images/bukugentayangan.jpg",
+            "assets/images/gentayangan2.jpg"
         ],
-        desc: "Riwayat singkat umat manusia.",
-        review: "Membuka wawasan tentang sejarah evolusi kita. Ditulis dengan narasi yang memikat sehingga sejarah terasa seperti cerita fiksi yang seru.",
+        desc: "Tak ada akhir buat perempuan-perempuan sihir. Sebagaimana kita tahu, cewek baik masuk surga, cewek bandel gentayangan. <br><br> There is no ending for witchy women. As we know, good girls go to heaven, while rebellious girls wander endlessly.",
+        review: "Buku Gentayangan karya Intan Paramadhita merupakan novel eksperimental bergaya choose your own adventure yang mengajak pembaca mengikuti perjalanan seorang perempuan yang membuat perjanjian dengan iblis demi mewujudkan keinginannya untuk berkeliling dunia. Dengan hadiah sepatu merah terkutuk, ia menjalani kehidupan “gentayangan”—berpindah dari satu kota ke kota lain tanpa benar-benar memiliki tempat pulang, sekaligus menghadapi berbagai kisah tentang identitas, batasan sosial, gender, dan kebebasan. Cerita yang dituturkan dengan sudut pandang orang kedua ini membuat pembaca seolah menjadi tokoh utama, memilih alur petualangan sendiri, dan menyadari bahwa di balik kebebasan tersebut terdapat keterbatasan, ilusi pilihan, serta pencarian makna tentang rumah, diri, dan keberadaan di dunia yang terus berubah. <br><br> The book <i>Gentayangan</i> by Intan Paramadhita is an experimental novel in the style of choose your own adventure that invites readers to follow the journey of a woman who makes a pact with the devil to fulfill her desire to travel the world. Gifted with a pair of cursed red shoes, she lives a “wandering” life—moving from one city to another without ever truly having a place to call home, while encountering stories about identity, social boundaries, gender, and freedom. Told from a second-person perspective, the narrative makes readers feel as if they are the protagonist, choosing their own path, while realizing that behind such freedom lie limitations, the illusion of choice, and an ongoing search for meaning about home, self, and existence in an ever-changing world",
         isFavorite: false
     },
     {
         id: 5,
         title: "Seorang Pria yang Melalui Duka dengan Mencuci Piring",
         cover: "assets/images/bukucucipiring.jpg",
-        landscape: "assets/images/bukucucipiring.jpg",
+        landscape: "assets/images/cucipiringld.jpg",
         portraits: [
-            "https://images.unsplash.com/photo-1518005020951-eccb494ad742?auto=format&fit=crop&q=80&w=300",
-            "https://images.unsplash.com/photo-1500382017468-9049fed747ef?auto=format&fit=crop&q=80&w=300",
-            "https://images.unsplash.com/photo-1437382836617-ee1cb9b99684?auto=format&fit=crop&q=80&w=300"
+            "assets/images/cucipiring1.jpeg",
+            "assets/images/cucipiring2.jpg",
+            "assets/images/cucipiring3.jpg"
         ],
-        desc: "Kisah keluarga, kehilangan, dan persahabatan.",
-        review: "Sangat menyentuh hati. Mengangkat sejarah Indonesia dengan cara yang indah namun menyayat hati. Sangat direkomendasikan.",
+        desc: "Bagi dunia, kehilangan satu orang hanyalah satu angka. Bagi orang yang ditinggalkan, kehilangan satu orang bisa jadi berarti kehilangan satu dunia. <br><br> For the world, losing one person is just a number. But for those left behind, losing one person can mean losing an entire world.",
+        review: "Buku Seorang Pria yang Melalui Duka dengan Mencuci Piring karya Andreas Kurniawan mengisahkan perjalanan emosional seorang pria dalam menghadapi kehilangan dan duka yang mendalam melalui cara-cara sederhana dalam keseharian, seperti mencuci piring. Dengan pendekatan yang hangat dan reflektif, buku ini menunjukkan bahwa proses berduka tidak selalu harus dramatis, melainkan bisa hadir dalam rutinitas kecil yang membantu seseorang perlahan menerima kenyataan dan menata kembali hidupnya. Dipadukan dengan sudut pandang psikologis, pembaca diajak memahami bahwa duka adalah proses yang personal dan valid, serta bahwa penyembuhan dapat ditemukan melalui kesadaran, penerimaan, dan keberanian untuk tetap menjalani hidup meskipun rasa kehilangan masih ada. <br><br> The book <i>Seorang Pria yang Melalui Duka dengan Mencuci Piring</i> by Andreas Kurniawan tells the emotional journey of a man coping with deep loss and grief through simple everyday activities, such as washing dishes. With a warm and reflective approach, the book shows that grieving does not always have to be dramatic, but can unfold quietly through small routines that help a person gradually accept reality and rebuild their life. Enriched with psychological insights, it invites readers to understand that grief is a personal and valid process, and that healing can be found through awareness, acceptance, and the courage to keep living even when the sense of loss still lingers.",
         isFavorite: false
     },
     {
         id: 6,
         title: "Janji",
         cover: "assets/images/bukujanji.jpeg",
-        landscape: "assets/images/bukujanji.jpeg",
+        landscape: "assets/images/janjild.png",
         portraits: [
-            "https://images.unsplash.com/photo-1614730321146-b6fa6a46bcb4?auto=format&fit=crop&q=80&w=300",
-            "https://images.unsplash.com/photo-1614729939124-032f0b56c9ce?auto=format&fit=crop&q=80&w=300",
-            "https://images.unsplash.com/photo-1534447677768-be436bb09401?auto=format&fit=crop&q=80&w=300"
+            "assets/images/janji1.webp",
+            "assets/images/janji2.jpg",
+            "assets/images/janji3.jpg"
         ],
-        desc: "Epik fiksi ilmiah tentang politik dan ekologi di planet gurun.",
-        review: "Mahakarya sci-fi. Pembangunan dunianya sangat detail dan intrik politiknya brilian.",
+        desc: "kata orang bijak dulu, kau akan lebih menyesal bukan karena kau melakukan sesuatu dan ternyata itu gagal atau keliru. kau akan lebih menyesal saat kau tidak pernah melakukan sesuatu mengingat betapa tidak beraninya kau mengambil keputusan. <br><br> As the wise once said, you will regret not the things you tried that failed or went wrong, but the things you never dared to do—remembering how you lacked the courage to make a decision.",
+        review: "Buku Janji karya Tere Liye mengisahkan perjalanan emosional tentang kesetiaan, penebusan, dan makna sebuah janji yang dipegang teguh dalam berbagai keadaan. Melalui alur cerita yang penuh misteri dan refleksi kehidupan, novel ini menggambarkan bagaimana setiap tokoh dihadapkan pada pilihan sulit, masa lalu yang membekas, serta konsekuensi dari keputusan yang mereka ambil. Dengan gaya penceritaan yang khas dan menyentuh, Tere Liye mengajak pembaca merenungkan tentang arti tanggung jawab, keikhlasan, dan kekuatan untuk tetap berpegang pada janji meskipun waktu dan keadaan terus berubah. <br><br> The book <i>Janji</i> by Tere Liye tells an emotional journey about loyalty, redemption, and the meaning of a promise that is upheld through all circumstances. Through a storyline filled with mystery and life reflections, the novel portrays how each character faces difficult choices, lingering pasts, and the consequences of their decisions. With a distinctive and touching narrative style, Tere Liye invites readers to reflect on the values of responsibility, sincerity, and the strength to hold on to a promise despite the passage of time and changing situations.",
         isFavorite: false
     }
 ];
